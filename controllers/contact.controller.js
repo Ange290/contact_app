98cf1e5b8e843f8bc74e93fd9bef6ad48b4f854b
Ange290 <uange209@gmail.com>
2024-04-02 10:11:24 +0200

    Update Feature

--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -7,9 +7,17 @@ const bookController ={
     createContact: async(req,res)=>{
         try {
             const newContact = await contactModel.create(req.body);
-            res.status(201).json({message:'Created Successfully', contacts: newContact})
+            res.status(201).json({message:'Created Successfully', contact: newContact})
         } catch (error) {
             res.status(500).json({message:'Error in creating Contact', Error: error.message})
+        }
+    }, 
+    getContact: async(re,res)=>{
+        try {
+            const getConatcts =await contactModel.find()
+            res.status(200).json({message: 'All Contact', contact:getConatcts})
+        } catch (error) {
+          res.status(500).json({message:'Error in get contact', Error: error.message})  
         }
     }
 }
